0a042576b8cd6d397349025051442d7524f18de8
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-02-20 06:53:14 +0000

    updeta4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,10 +27,14 @@ class BooksController < ApplicationController
   end
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice]="You have updated book successfully"
-    redirect_to book_path(@book.id)
+    if@book.update(book_params)
+      flash[:notice]="You have updated book successfully"
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
   end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -40,6 +44,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:image, :title, :body)
+    params.require(:book).permit(:image,:title,:body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :is_matching_login_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -6,7 +7,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @books=Book.all
+    @users=User.all
   end
 
   def edit
@@ -15,13 +16,23 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    flash[:notice]="You have updated user successfully"
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+       flash[:notice]="You have updated user successfully"
+       redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
   
   private
   def user_params
     params.require(:user).permit(:name, :profile_image,:introduction)
+  end
+  
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to users_path
+    end
   end
 end
